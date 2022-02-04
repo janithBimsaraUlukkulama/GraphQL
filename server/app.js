5ffb8b876cd0dbe64a632990a09e10e8bff5b9c3
janithBimsaraUlukkulama <jbimsaraulukkulama@outlook.com>
2022-02-04 19:29:11 +0530

    resolve function

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
+    schema
+}));
 
-}))
-
-app.listen(4000,()=>{
+app.listen(4000, () => {
     console.log('Server run in port 400');
 })
